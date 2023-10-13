3b792c165596a269b8ee72b35d1f8903c8373fe9
Pavithral5122 <145579876+Pavithral5122@users.noreply.github.com>
2023-10-13 20:31:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const obj1 = {name:"person 1", age:5};
+const keys1 = Object.keys(obj1).sort();
+console.log(keys1);
+const obj2 = {age:5,name:"person 1"};
+const keys2 = Object.keys(obj2).sort();
+console.log(keys2);
+if(JSON.stringify(keys1)== JSON.stringify(keys2)){
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
