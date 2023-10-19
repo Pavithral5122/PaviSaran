e2f80dc6152c5e3e11d92f213adfffe5fb7e069e
Pavithral5122 <145579876+Pavithral5122@users.noreply.github.com>
2023-10-19 14:30:18 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,42 @@
+let myResume={
+    "basics": {
+      "name": "Pavithra Loganathan",
+      "email": "pavithra.l050122@gamil.com",
+      "phone": 9042352981,
+      "degree": "B.E",
+      "location": {
+        "address": " 278,anaicut road,poondi village",
+        "pinCode": 632513,
+        "city": "Ranipet",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    },
+    
+    "Education": [
+      {
+        "Institusion":"Panimalar Institute Of Technology",
+        "Department": "ECE",
+        "StudyType": "fulltime",
+        "Batch start year": 2019,
+        "Batch end year": 2023,
+        "CGPA": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        
+      }
+      
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ]
+  }
+  console.log(myResume);
+  
